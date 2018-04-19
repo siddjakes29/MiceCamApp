35dcc02b113684a8ce7ac624b59a942d038de7df
siddjakes29 <wolfman29@gmail.com>
2018-04-19 16:11:53 -0400

    Add files via upload

---
+++ Mice_TrailTracking_ThreeCameras/CamAppHW/flircam/flircam_hw.py
@@ -0,0 +1,94 @@
+'''
+Created on Aug 9, 2017
+
+@author: Hao Wu
+'''
+
+from ScopeFoundry import HardwareComponent
+from .flircam_dev import FLIRCamDev
+
+class FLIRCamHW(HardwareComponent):
+    '''
+    Hardware Component Class for receiving AI input for breathing, licking etc
+    '''
+    
+    name='flircam'
+
+    def setup(self,camera_sn = ''):
+        self.settings.New(name='camera_sn',dtype=str,initial=camera_sn,ro=True)
+        self.settings.New(name ='model', dtype = str, initial ='N/A',ro = True)
+        self.settings.New(name = 'width', dtype = int, initial = 1920, ro = True)
+        self.settings.New(name = 'height', dtype = int, initial = 1200, ro = True)
+        self.settings.New(name = 'auto_exposure', dtype = bool, initial = True, ro = False)
+        self.settings.New(name = 'exposure_time', dtype = float, initial = 1000, ro = False)
+        self.settings.New(name = 'video_mode', dtype = int, initial = 0, ro = False, vmin = 0, vmax = 2)
+        self.settings.New(name = 'frame_rate', dtype = float, initial = 30, ro = False, vmin = 0, vmax = 100)
+        
+        
+                
+    def connect(self):
+        #connect to the camera device
+        self._dev=FLIRCamDev(self.settings.camera_sn.value())
+        
+        #define read functions
+        self.settings.model.hardware_read_func = self._dev.get_model
+        self.settings.width.hardware_read_func = self._dev.get_width
+        self.settings.height.hardware_read_func = self._dev.get_height
+        self.settings.auto_exposure.hardware_read_func = self._dev.get_auto_exposure
+        self.settings.exposure_time.hardware_read_func = self._dev.get_exp
+        self.settings.video_mode.hardware_read_func = self._dev.get_video_mode
+        self.settings.frame_rate.hardware_read_func = self._dev.get_frame_rate
+        
+        #define set functions
+        self.settings.auto_exposure.hardware_set_func = self._dev.set_auto_exposure
+        self.settings.exposure_time.hardware_set_func = self._dev.set_exp
+        self.settings.video_mode.hardware_set_func = self._dev.set_video_mode
+        self.settings.frame_rate.hardware_set_func = self._dev.set_frame_rate
+        
+        #read camera info
+        self.read_from_hardware()
+        
+        
+    
+    def start(self):
+        self._dev.start()
+    
+    def stop(self):
+        self._dev.stop()
+        
+    def read(self):
+        return self._dev.read()
+    
+    def empty(self):
+        return self._dev.empty()
+    
+    def write(self):
+        self._dev.write()
+        
+    def to_numpy(self,image):
+        return self._dev.to_numpy(image)
+        
+    def disconnect(self):
+        '''
+        need bug fix for pointer issues
+        '''
+        try:
+            # remove read functions
+            self.settings.model.hardware_read_func = None
+            self.settings.width.hardware_read_func = None
+            self.settings.height.hardware_read_func = None
+            self.settings.auto_exposure.hardware_read_func = None
+            self.settings.exposure_time.hardware_read_func = None
+            self.settings.video_mode.hardware_read_func = None
+            self.settings.frame_rate.hardware_read_func = None
+            #remove set functions
+            self.settings.auto_exposure.hardware_set_func = None
+            self.settings.exposure_time.hardware_set_func = None
+            self.settings.video_mode.hardware_set_func = None
+            self.settings.frame_rate.hardware_set_func = None
+            
+            self._dev.close()
+            del self._dev
+            
+        except AttributeError:
+            pass
---
+++ Mice_TrailTracking_ThreeCameras/Camera_Acquisition.py
@@ -0,0 +1,80 @@
+'''
+Created on April 19, 2018
+
+@author: Sid/J 
+'''
+
+from ScopeFoundry.base_app import BaseMicroscopeApp
+from ScopeFoundry.helper_funcs import sibling_path, load_qt_ui_file
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# Define your App that inherits from BaseMicroscopeApp
+class CamApp(BaseMicroscopeApp):
+    
+    # this is the name of the microscope that ScopeFoundry uses 
+    # when displaying your app and saving data related to it    
+    name = 'MiceTracking'
+
+    # You must define a setup() function that adds all the 
+    # capabilities of the microscope and sets default settings    
+    def setup(self):
+
+        #Add App wide settings
+        #self.settings.New('test1', dtype=int, initial=0)
+        
+        #Add hardware components
+        print("Adding Hardware Components")
+        from MiceTrackingHW.flircam.flircam_hw import FLIRCamHW
+        Front_cam = FLIRCamHW(self)
+        Front_cam.settings.camera_sn.update_value('17550014')
+        Front_cam.name = 'Front_cam'
+        Top_cam = FLIRCamHW(self)
+        Top_cam.settings.camera_sn.update_value('16307752')
+        #Side_cam.name = 'Top_cam'
+        #Side_cam = FLIRCamHW(self)
+        #Side_cam.settings.camera_sn.update_value('15420264')
+        #Side_cam.name = 'Side_cam'
+        self.add_hardware(Top_cam)
+        self.add_hardware(Front_cam)
+        #self.add_hardware(Side_cam)
+        
+        from MiceTrackingHW.flircam.flirrec_hw import FLIRRecHW
+        self.add_hardware(FLIRRecHW(self))
+               
+        #Add Measurement components
+        print("Create Measurement objects")
+        from MiceTrackingMS.MiceTrack import MiceTrackMeasure
+        self.add_measurement(MiceTrackMeasure(self))
+        
+        # Connect to custom gui
+        
+        # load side panel UI
+        
+        # show ui
+        self.ui.show()
+        self.ui.activateWindow()
+        
+        # load side panel UI        
+        #quickbar_ui_filename = sibling_path(__file__, "quickbar.ui")        
+        #self.add_quickbar( load_qt_ui_file(quickbar_ui_filename) )
+        
+        # Connect quickbar ui elements to settings
+        # self.quickbar.foo_pushButton.connect(self.on_foo)
+        
+        # load default settings from file
+        #self.settings_load_ini(sibling_path(__file__, "defaults.ini"))
+        
+if __name__ == '__main__':
+    
+    import sys
+    app = CamApp(sys.argv)
+    
+    app.hardware['Side_cam'].connected.update_value(True)
+    app.hardware['Top_cam'].connected.update_value(True)
+    app.hardware['Tracking'].connected.update_value(True)
+    #app.hardware['daq_timer'].connected.update_value(True)
+    
+    sys.exit(app.exec_())
+    
